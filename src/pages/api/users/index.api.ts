a99a33718a07186c9e7a290e28363e9f931398ec
Wesed <dev.wfeduardo@gmail.com>
2023-10-11 15:01:22 -0300

    Cadastro de usuario - manipulando cookie

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,8 +11,26 @@ export default async function handler(
   }
 
   const { name, username } = req.body
+
+  // verifica se o username ja existe
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({ error: 'Username already taken.' })
+  }
+
   const user = await prisma.user.create({
     data: { name, username },
+  })
+
+  /* os cookies trafegam atraves do header da resposta */
+  setCookie({ res }, '@dev-call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return res.status(201).json(user)

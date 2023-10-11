b40f686f761dc3e8421d9e1b632fa1a46e7a248e
Wesed <dev.wfeduardo@gmail.com>
2023-10-11 15:32:26 -0300

    Conexao com calendario - pagina de conexao

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -57,6 +57,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
       if (err instanceof AxiosError && err?.response?.data) {
         alert(err.response.data.error)
@@ -105,8 +107,8 @@ export default function Register() {
           )}
         </label>
 
-        <Button type="submit">
-          Próximo passo
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Carregando...' : 'Próximo passo'}
           <ArrowRight />
         </Button>
       </Form>

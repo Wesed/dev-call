d85916c673791f5aebe670de92e365b937ee943d
Wesed <dev.wfeduardo@gmail.com>
2023-10-17 17:29:22 -0300

    Calendario e disponibilidade - Formulario de confirmação

--- src/pages/schedule/[username]/components/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/components/ScheduleForm/ConfirmStep/index.tsx
@@ -1,14 +1,34 @@
 import { CalendarBlank, Clock } from 'phosphor-react'
-import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const confirmFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 caracteres.' }),
+  email: z.string().email({ message: 'Digite um email válido.' }),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>
 
 export function ConfirmStep() {
-  function handleConfirmScheduling() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+  function handleConfirmScheduling(data: ConfirmFormData) {
     return null
   }
 
   return (
-    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
@@ -22,7 +42,12 @@ export function ConfirmStep() {
 
       <label>
         <Text size="sm">Nome completo</Text>
-        <TextInput placeholder="Seu nome" crossOrigin="anonymous" />
+        <TextInput
+          placeholder="Seu nome"
+          crossOrigin="anonymous"
+          {...register('name')}
+        />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
       </label>
 
       <label>
@@ -31,12 +56,16 @@ export function ConfirmStep() {
           type="email"
           placeholder="youremail@example.com"
           crossOrigin="anonymous"
+          {...register('email')}
         />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
       </label>
 
       <label>
         <Text size="sm">Observações</Text>
-        <TextArea />
+        <TextArea {...register('observations')} />
       </label>
 
       <FormActions>
@@ -44,7 +73,9 @@ export function ConfirmStep() {
           Cancelar
         </Button>
 
-        <Button type="submit">Cancelar</Button>
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Confirmando...' : 'Confirmar'}
+        </Button>
       </FormActions>
     </ConfirmForm>
   )

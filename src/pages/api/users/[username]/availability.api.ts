9da7e0f52aadccb3ea71ff2175b32df8489b2c61
Wesed <dev.wfeduardo@gmail.com>
2023-10-19 15:00:32 -0300

    Calendario e disponibilidade - rota de horarios disponiveis | retornando horarios disponiveis de meia em meia hora

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,62 @@
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date no provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User does not exust.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  // se a data estiver no passado, retorna um array vazio (sem disponibilidade)
+  if (isPastDate) {
+    return res.status(200).json({ availability: [] })
+  }
+
+  // se o dia escolhido tiver disponibilidade
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.status(200).json({ availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  const startHour = time_start_in_minutes / 60 // 08:00
+  const endHour = time_end_in_minutes / 60 // 18:00
+
+  const possibleTimes = Array.from({ length: (endHour - startHour) * 2 }).map(
+    (_, i) => {
+      return startHour + i * 0.5
+    },
+  )
+
+  return res.json({ possibleTimes })
+}
